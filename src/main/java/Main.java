7d4c2095076b0481facef4230ece210b8d472e23
WildWindXY <TylorMeng@gmail.com>
2023-09-27 23:45:45 -0400

    API call to DeepL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,42 @@
 package main.java;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        try {
+            URL url = new URL("https://api-free.deepl.com/v2/translate");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "DeepL-Auth-Key ce7a4edb-7949-68cb-4f80-49104d53fe70:fx");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setDoOutput(true);
 
+            String requestBody = "text=Hello, world!&target_lang=DE";
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = requestBody.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            int responseCode = connection.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            System.out.println("Response Code: " + responseCode);
+            System.out.println("Response Data: " + response);
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
