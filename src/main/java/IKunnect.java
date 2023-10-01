24f6ebd24722f3b48e138bbbf88717b81cdd547a
Schrodingers-Neko <liujiayou0811@gmail.com>
2023-09-30 21:32:43 -0400

    changed encoding to GB18030 as opposed to the recommended UTF-8 in API documentation because they UTF-8 does not work.
    added a proposed plan for project file structure

--- src/main/java/IKunnect.java
+++ src/main/java/IKunnect.java
@@ -1,14 +1,58 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
-public class IKunnect {
+    public class IKunnect {
 
-    public static void main(String[] args) {
-        try {
+        private static final Logger logger = LogManager.getLogger(IKunnect.class);
+        public static void main(String[] args) {
+
+            try {
+                String text = "鸡你太美";
+                String sourceLang = "ZH";
+                String targetLang = "EN-US";
+                /*String encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8.toString());
+                Using UTF-8 as directed by the DeepL API returned this for "鸡你太美"
+                Response Data: {"translations":[{"detected_source_language":"ZH","text":"Last name: ?"}]}
+                While GB18030 give the correct response:
+                Response Data: {"translations":[{"detected_source_language":"ZH","text":"Chicken, you're so beautiful."}]}
+                However, for single character requests it always returns
+                Response Data: {"translations":[{"detected_source_language":"ZH","text":"å1¤7"}]}
+                Somehow Chinese full-width punctuations also causes the entire thing to be unreadable.
+                String text = "你好世界";
+                Response Data: {"translations":[{"detected_source_language":"ZH","text":"ä½ å¥½ä¸çï¼1¤7"}]}
+                I blame DeepL API*/
+                String encodedText = URLEncoder.encode(text, Charset.forName("GB18030").toString());
+                String requestBody = String.format("text=%s&source_lang=%s&target_lang=%s", encodedText, sourceLang, targetLang);
+
+                HttpURLConnection connection = getHttpURLConnection(requestBody);
+
+                int responseCode = connection.getResponseCode();
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                System.out.println("Response Code: " + responseCode);
+                System.out.println("Response Data: " + response);
+                connection.disconnect();
+            } catch (Exception e) {
+                logger.error("An error occurred in the main method", e);
+            }
+        }
+
+        private static HttpURLConnection getHttpURLConnection(String requestBody) throws IOException {
             URL url = new URL("https://api-free.deepl.com/v2/translate");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
@@ -16,25 +60,12 @@ public class IKunnect {
             connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             connection.setDoOutput(true);
 
-            String requestBody = "text=哟&source_lang=ZH&target_lang=EN";
+            // Specify the UTF-8 character encoding for the request body
             try (OutputStream os = connection.getOutputStream()) {
                 byte[] input = requestBody.getBytes(StandardCharsets.UTF_8);
-                os.write(input, 0, input.length);
+                os.write(input);
             }
 
-            int responseCode = connection.getResponseCode();
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            System.out.println("Response Code: " + responseCode);
-            System.out.println("Response Data: " + response);
-            connection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
+            return connection;
         }
     }
-}
