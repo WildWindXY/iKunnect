c0b2d3208e51da9cdb9b66f08035d376ff8fb71a
WildWindXY <TylorMeng@gmail.com>
2023-11-07 18:48:42 -0500

    Import JLine3

--- src/main/java/common/packet/PacketClientMessage.java
+++ src/main/java/common/packet/PacketClientMessage.java
@@ -1,9 +1,8 @@
 package common.packet;
 
-public class PacketClientMessage implements Packet{
+public class PacketClientMessage implements Packet {
     private final String message;
     private final long recipientID;
-
 
 
     public PacketClientMessage(long recipientID, String message) {
