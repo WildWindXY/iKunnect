5eac933db586c0a53607c6f33eec82c469a65797
hannliu <liuhannah@gmail.com>
2023-12-02 16:14:09 -0500

    Added popups

--- src/main/java/client/view/SignupView.java
+++ src/main/java/client/view/SignupView.java
@@ -207,6 +207,13 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
             passwordBuilder.delete(0, passwordBuilder.length());
             passwordField.setText("");
             String password = signupViewModel.getState().getPassword();
+
+            // Check if the password is valid
+            boolean isValidPassword = controller.checkPassword(password);
+            String message = isValidPassword ? "Password is valid." : "Password is invalid.";
+            // Show the dialog
+            JOptionPane.showMessageDialog(null, message, "Password Validation", JOptionPane.INFORMATION_MESSAGE);
+
             System.out.println("Sign up:" + password + " Is password valid? :" + controller.checkPassword(password));
         });
 
